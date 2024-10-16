706a9dab936bcd38655789fa038dccd6e16f1117
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-10-16 15:56:47 +0100

    PRODUCTION MODE - Version 1.0.0

--- resources/views/Layouts/1.blade.php
+++ resources/views/Layouts/1.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGIN | Daily Visitor Entry</title>
+    <title>Dashboard | Visitor Management System</title>
+    <link rel="icon" href="{{ asset('Images/bg-x.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('Css/Styles.css') }}">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://fonts.Guy Agassiapis.com/css?family=Montserrat:300,600" />
--- resources/views/Pages/Login.blade.php
+++ resources/views/Pages/Login.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGIN | Daily Visitor Entry</title>
+    <title>LOGIN | Visitor Management System</title>
     <link rel="stylesheet" href="{{ asset('Css/Styles.css') }}">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:300,600" />
+    <link rel="icon" href="{{ asset('Images/bg-x.png') }}" type="image/x-icon">
 </head>
 <body class="Login">
     <img class="company-logo" src="{{ 'Images/LTT -DEPASA Logo.png' }}" alt="">
@@ -18,7 +19,10 @@
         <div class="container">
             <form class="LoginForm">
                 @csrf
-                <h1>Login</h1>
+                {{-- <h1>Login</h1> --}}
+                <div class="register">
+                    <p>VISITOR MANGEMENT SYSTEM</p>
+                </div>
                 <p class="Error"></p>
                 <div class="input-box">
                     <input type="email" name="Email" placeholder="Email">
@@ -30,13 +34,10 @@
                     <i class='bx bx-lock-alt' ></i>
                 </div>
                 <div class="remember"> 
-                    <a href="#">L.T.T | DEPASA MARINE INT'L</a>
+                    <a href="#">Security Department</a>
                 </div>
             </form>
             <button class="btn LoginButton">Login</button>
-            <div class="register">
-                <p>Security Department <br><br> <a href="#">DAILY VISITOR ENTRY</a></p>
-            </div>
         </div>
     </div>
     <script src="{{ asset('Js/Loader.js') }}"></script>
